123af30851f6de9768a19838237b5bbf3f0f0e83
kravetsd <kdykrg@gmail.com>
2022-01-29 20:09:44 +0300

    Refactored a bit

--- decoupling/methodSetrules.go
+++ decoupling/methodSetrules.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+type duration int
+
+func (d *duration) notify() {
+	fmt.Printf("Sending notification in %d \n", d)
+}
+
+type notifier interface {
+	notify()
+}
+
+type user struct {
+	name  string
+	email string
+}
+
+func (u *user) notify() {
+	fmt.Printf("Notify user %s via it's email %s\n", u.name, u.email)
+}
+
+func sendNotification(n notifier) {
+	n.notify()
+}
 func main() {
-    fmt.Println("Hello another go")
+	us := user{"Dima", "admin@example.com"}
+	sendNotification(&us)
+
+	duration(42).notify()
 }
