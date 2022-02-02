302045ad11f8eced1efb30793b7036d3d0f029d7
kravetsd <kdykrg@gmail.com>
2022-02-03 00:17:08 +0300

    Finalized decoupling with interfaces.

--- design/decoupleWithInterfaces.go
+++ design/decoupleWithInterfaces.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"io"
+	"math/rand"
+	"time"
+)
+
 type Data struct {
-	line string
+	Line string
 }
 
 type Storer interface {
@@ -47,5 +55,107 @@ func Copy(ps PullStorer, batch int) error {
 		if err != nil {
 			return err
 		}
+	}
+}
+
+//Now lets instanciate these interfaces:
+
+type Xenia struct {
+	Host    string
+	Timeout time.Duration
+}
+
+func (x *Xenia) Pull(d *Data) error {
+	switch rand.Intn(10) {
+	case 1, 9:
+		return io.EOF
+	case 5:
+		return errors.New("Error reading data from Xenia")
+	default:
+		d.Line = "Data"
+		fmt.Println("In:", d.Line)
+		return nil
+	}
+}
+
+type Pillar struct {
+	Host    string
+	Timeout time.Duration
+}
+
+func (p *Pillar) Store(d *Data) error {
+	fmt.Println("Out:", d.Line)
+	return nil
+}
+
+type System1 struct {
+	Pillar
+	Xenia
+}
+
+// ----anothther example of storer and puller:
+
+type Bob struct {
+	Host    string
+	Timeout time.Duration
+}
+
+func (b *Bob) Pull(d *Data) error {
+	switch rand.Intn(10) {
+	case 1, 9:
+		return io.EOF
+	case 5:
+		return errors.New("Error reading data from Bob")
+	default:
+		d.Line = "Data"
+		fmt.Println("In:", d.Line)
+		return nil
+	}
+}
+
+type Ann struct {
+	Host    string
+	Timeout time.Duration
+}
+
+func (a *Ann) Store(d *Data) error {
+	fmt.Println("Out:", d.Line)
+	return nil
+}
+
+type System2 struct {
+	Bob
+	Ann
+}
+
+func main() {
+	sys1 := System1{
+		Xenia: Xenia{
+			Host:    "localhost:8000",
+			Timeout: time.Second,
+		},
+		Pillar: Pillar{
+			Host:    "localhost:9000",
+			Timeout: time.Second,
+		},
+	}
+
+	sys2 := System2{
+		Bob: Bob{
+			Host:    "localhost:8000",
+			Timeout: time.Second,
+		},
+		Ann: Ann{
+			Host:    "localhost:9000",
+			Timeout: time.Second,
+		},
+	}
+
+	if err := Copy(&sys1, 3); err != io.EOF {
+		fmt.Println(err)
+	}
+
+	if err := Copy(&sys2, 3); err != io.EOF {
+		fmt.Println(err)
 	}
 }
